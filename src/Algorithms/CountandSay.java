11f8e79e39938b6a917280d38e6cf0a40370ce5c
’rylynnczxshuaidai’ <477895675@qq.com>
2020-04-01 14:49:40 +0800

    做的一个dp算法。。。好在是啃下来了

--- src/Algorithms/CountandSay.java
+++ src/Algorithms/CountandSay.java
@@ -2,4 +2,39 @@ package Algorithms;
 
 public class CountandSay {
 
+    public String countAndSay(int n){
+
+        if (n<=0) return "";
+        String[] str = new String[n];
+        str[0] = "1";
+        for(int i = 1 ; i < n ; i++){
+            str[i] = covert(str[i-1]);
+        }
+
+        return str[n-1];
+
+    }
+
+    public String covert(String str){
+
+        StringBuilder sb = new StringBuilder();
+        int count = 1;
+        for(int i = 0; i < str.length(); i++){
+            if(i < str.length() - 1 && str.charAt(i) == str.charAt(i+1)){
+                count++;
+            }else {
+                sb.append(count).append(str.charAt(i));
+                count = 1;
+            }
+        }
+
+        return sb.toString();
+
+    }
+
+    public static void main(String[] args) {
+        CountandSay c = new CountandSay();
+        System.out.println(c.countAndSay(2));
+    }
+
 }
