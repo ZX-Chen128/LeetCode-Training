8c04e8859c652c3baa6a963bf647ac889efb66e6
’rylynnczxshuaidai’ <477895675@qq.com>
2020-04-10 18:33:55 +0800

    可重入锁

--- src/test/test.java
+++ src/test/test.java
@@ -1,32 +1,64 @@
 package test;
 
-import Algorithms.AddTwoNumber;
-
-import java.io.*;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class test {
 
-    /*
-    private volatile int count = 0;
+    AtomicReference<Thread> cas = new AtomicReference<Thread>();
+    private int count = 0;
 
-    public synchronized void increment(){
-        count++;
+    public void lock(){
+
+        Thread thread = Thread.currentThread();
+        if(thread == cas.get()){
+            count++;
+            System.out.println("CurrenThread :"+thread+"第"+(count+1)+"次获取线程");
+            return;
+        }
+
+        while(!cas.compareAndSet(null,thread)){
+            System.out.println("CurrenThread :"+thread+"自旋中...");
+        }
+
+        System.out.println("CurrenThread :"+thread+"第"+(count+1)+"次获取线程");
+
     }
 
-    public int getCount(){
-        return count;
+    public void unlock(){
+
+        Thread cur = Thread.currentThread();
+
+        if(cur == cas.get()){
+            if(count>0){
+                count--;
+                System.out.println("CurrenThread :"+cur+"释放了1次资源，现在是第"+(count+1)+"次获取线程");
+            } else {
+                cas.compareAndSet(cur,null);
+                System.out.println("CurrenThread :"+cur+"释放锁资源");
+            }
+        }
+
     }
-     */
 
-    public AtomicInteger count = new AtomicInteger(0);
+    public static void main(String[] args) throws InterruptedException {
 
-    public void increment(){
-        count.incrementAndGet();
-    }
+        test t1 = new test();
 
-    public int getCount(){
-        return count.get();
+        new Thread(()->{
+                t1.lock();
+                t1.lock();
+                try {
+                    Thread.sleep(5);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                t1.unlock();
+                t1.unlock();
+
+        },"臭臭").start();
+
+        new Thread(()-> t1.lock(),"香香").start();
+
     }
 
 }
