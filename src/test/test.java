321da4a3ef86c72880bc3e066727ee4c8e5c8052
’rylynnczxshuaidai’ <477895675@qq.com>
2020-04-12 21:00:12 +0800

    做的一个dp算法。。。好在是啃下来了

--- src/test/test.java
+++ src/test/test.java
@@ -1,64 +1,41 @@
 package test;
 
-import java.util.concurrent.atomic.AtomicReference;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ *
+ * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+ *
+ * 示例:
+ *
+ * 给定 nums = [2, 7, 11, 15], target = 9
+ *
+ * 因为 nums[0] + nums[1] = 2 + 7 = 9
+ * 所以返回 [0, 1]
+ */
 
 public class test {
 
-    AtomicReference<Thread> cas = new AtomicReference<Thread>();
-    private int count = 0;
+    public int[] test(int[] nums , int target){
 
-    public void lock(){
-
-        Thread thread = Thread.currentThread();
-        if(thread == cas.get()){
-            count++;
-            System.out.println("CurrenThread :"+thread+"第"+(count+1)+"次获取线程");
-            return;
+        Map<Integer,Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if(hashMap.containsKey(target-nums[i]))
+                return new int[]{hashMap.get(target-nums[i]),i};
+            hashMap.put(nums[i],i);
         }
-
-        while(!cas.compareAndSet(null,thread)){
-            System.out.println("CurrenThread :"+thread+"自旋中...");
-        }
-
-        System.out.println("CurrenThread :"+thread+"第"+(count+1)+"次获取线程");
-
+        return null;
     }
 
-    public void unlock(){
-
-        Thread cur = Thread.currentThread();
-
-        if(cur == cas.get()){
-            if(count>0){
-                count--;
-                System.out.println("CurrenThread :"+cur+"释放了1次资源，现在是第"+(count+1)+"次获取线程");
-            } else {
-                cas.compareAndSet(cur,null);
-                System.out.println("CurrenThread :"+cur+"释放锁资源");
-            }
+    public static void main(String[] args) {
+        int[] nums = {2,7,11,15};
+        int target = 9;
+        test t = new test();
+        for(int i : t.test(nums,target)){
+            System.out.println(i);
         }
-
-    }
-
-    public static void main(String[] args) throws InterruptedException {
-
-        test t1 = new test();
-
-        new Thread(()->{
-                t1.lock();
-                t1.lock();
-                try {
-                    Thread.sleep(5);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                t1.unlock();
-                t1.unlock();
-
-        },"臭臭").start();
-
-        new Thread(()-> t1.lock(),"香香").start();
-
     }
 
 }
