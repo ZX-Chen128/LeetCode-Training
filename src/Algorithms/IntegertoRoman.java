21b1a68a8fa88bc69e27cf17a4cd78a7e42d98cf
’rylynnczxshuaidai’ <477895675@qq.com>
2020-04-17 13:15:07 +0800

    阿拉伯数字转罗马数字，新实现

--- src/Algorithms/IntegertoRoman.java
+++ src/Algorithms/IntegertoRoman.java
@@ -1,22 +1,45 @@
 package Algorithms;
 
+/**
+ *     public String intToRoman(int num) {
+ *         int[] nums = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+ *         String[] romans = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+ *         StringBuilder sb = new StringBuilder();
+ *         int index = 0;
+ *         while(index < 13){
+ *             while(num >= nums[index]){
+ *                 sb.append(romans[index]);
+ *                 num -= nums[index];
+ *             }
+ *             index++;
+ *         }
+ *
+ *         return sb.toString();
+ *
+ *     }
+ */
+
 public class IntegertoRoman {
 
     public String intToRoman(int num) {
-        int[] nums = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-        String[] romans = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-        StringBuilder sb = new StringBuilder();
-        int index = 0;
-        while(index < 13){
-            while(num >= nums[index]){
-                sb.append(romans[index]);
+        StringBuffer str = new StringBuffer();
+        int[] nums = {1,4,5,9,10,40,50,90,100,400,500,900,1000};
+        String[] strs = {"I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"};
+        int index = 12;
+        while(num>=1){
+            if(num>=nums[index]){
+                str.append(strs[index]);
                 num -= nums[index];
+            } else {
+                index--;
             }
-            index++;
         }
+        return str.toString();
+    }
 
-        return sb.toString();
-
+    public static void main(String[] args) {
+        IntegertoRoman i = new IntegertoRoman();
+        System.out.println(i.intToRoman(1994));
     }
 
 }
