55bb42756bebca728747b1136c96543ded7de354
’rylynnczxshuaidai’ <477895675@qq.com>
2020-04-24 15:52:39 +0800

    选择排序和冒泡排序

---
+++ src/Algorithms/BasicAlgorithms/BubbleSort.java
@@ -0,0 +1,23 @@
+package Algorithms.BasicAlgorithms;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public int[] bubbleSort(int [] arr) {
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = 0; j < arr.length-i-1; j++) {
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        BubbleSort b = new BubbleSort();
+        System.out.println(Arrays.toString(b.bubbleSort(new int[]{5,4,3,2,1})));
+    }
+}
---
+++ src/Algorithms/BasicAlgorithms/SelectSort.java
@@ -0,0 +1,28 @@
+package Algorithms.BasicAlgorithms;
+
+import java.util.Arrays;
+
+public class SelectSort {
+    public int[] selectSort(int [] arr){
+        for (int i = 0; i < arr.length; i++) {
+            int index = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if(arr[j]<arr[index]){
+                    index = j;
+                }
+            }
+            if(index != i) {
+                int temp = arr[index];
+                arr[index] = arr[i];
+                arr[i] = temp;
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        SelectSort s = new SelectSort();
+        int[] result = s.selectSort(new int[]{2,14,44,8,5});
+        System.out.println(Arrays.toString(result));
+    }
+}
