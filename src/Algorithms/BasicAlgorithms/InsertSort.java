53ab68d49a4beb2ca6133ebf219cac1f11c46dc3
’rylynnczxshuaidai’ <477895675@qq.com>
2020-04-30 16:56:05 +0800

    4.30算法，插入排序

--- src/Algorithms/BasicAlgorithms/InsertSort.java
+++ src/Algorithms/BasicAlgorithms/InsertSort.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class InsertSort {
 
     public int[] insertSort(int[] nums) {
-        int i,j;
+        int i, j;
         int length = nums.length;
         for (i = 1; i < length; i++) {
             int temp = nums[i];
@@ -13,17 +13,17 @@ public class InsertSort {
                 if (temp > nums[j]) {
                     break;
                 } else {
-                    nums[j+1] = nums[j];
+                    nums[j + 1] = nums[j];
                 }
             }
-            nums[j+1] = temp;
+            nums[j + 1] = temp;
         }
         return nums;
     }
 
     public static void main(String[] args) {
         InsertSort is = new InsertSort();
-        System.out.println(Arrays.toString(is.insertSort(new int[]{5,4,3,2,1})));
+        System.out.println(Arrays.toString(is.insertSort(new int[]{5, 4, 3, 2, 1})));
     }
 
 }
