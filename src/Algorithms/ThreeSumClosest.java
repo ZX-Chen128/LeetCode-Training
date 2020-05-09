bca9ac82e8fa5db228cf7fdeb5535d67ceb3f726
’rylynnczxshuaidai’ <477895675@qq.com>
2020-05-09 19:19:11 +0800

    5.9 最接近的三数之和

--- src/Algorithms/ThreeSumClosest.java
+++ src/Algorithms/ThreeSumClosest.java
@@ -3,23 +3,32 @@ package Algorithms;
 import java.util.Arrays;
 
 public class ThreeSumClosest {
-    public int threeSumClosest(int[] nums, int target) {
+    public static int threeSumClosest(int[] nums, int target) {
         Arrays.sort(nums);
-        int ans = nums[0] + nums[1] + nums[2];
-        for(int i=0;i<nums.length;i++) {
-            int start = i+1, end = nums.length - 1;
-            while(start < end) {
-                int sum = nums[start] + nums[end] + nums[i];
-                if(Math.abs(target - sum) < Math.abs(target - ans))
-                    ans = sum;
-                if(sum > target)
-                    end--;
-                else if(sum < target)
-                    start++;
-                else
-                    return ans;
+        int abs = nums[0] + nums[1] + nums[2];
+        for (int i = 0; i < nums.length; i++) {
+            int head = i + 1;
+            int back = nums.length - 1;
+            while(head < back){
+                int sum = nums[i] + nums[head] + nums[back];
+                if(Math.abs(target - abs) > Math.abs(target - sum)){
+                    abs = sum;
+                }
+                if(sum > target){
+                    back--;
+                } else if(sum < target){
+                    head++;
+                } else {
+                    return target;
+                }
             }
         }
-        return ans;
+        return abs;
     }
+
+    public static void main(String[] args) {
+        System.out.println(ThreeSumClosest.threeSumClosest(new int[]{-1,2,1,-4},1));
+    }
+
 }
+
