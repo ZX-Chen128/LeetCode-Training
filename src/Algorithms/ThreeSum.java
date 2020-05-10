6f02ed0463ef499f8755322fca87f7ebbaecfad2
’rylynnczxshuaidai’ <477895675@qq.com>
2020-05-10 17:36:10 +0800

    5.9 三数之和

---
+++ src/Algorithms/ThreeSum.java
@@ -0,0 +1,36 @@
+package Algorithms;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ThreeSum {
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        int len = nums.length;
+        Arrays.sort(nums);
+        for (int i = 0; i < len; i++) {
+            if(nums[i] > 0) break;
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+            int L = i + 1;
+            int R = len - 1;
+            while(L < R){
+                int sum = nums[i] + nums[L] + nums[R];
+                if(sum == 0){
+                    ans.add(Arrays.asList(nums[i],nums[L],nums[R]));
+                    while (L < R && nums[L] == nums[L+1]) L++;
+                    while (L < R && nums[R] == nums[R-1]) R--;
+                    L++;
+                    R--;
+                } else if (sum > 0) R--;
+                else L++;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(ThreeSum.threeSum(new int[]{-1, 0, 1, 2, -1, -4}));
+    }
+
+}
--- src/test/test.java
+++ src/test/test.java
@@ -1,28 +1,36 @@
 package test;
 
+import Algorithms.ThreeSumClosest;
+
+import java.util.Arrays;
+
 public class test {
-    private int i = 1;
-    public static String str = "outclass";
 
-    class InnerClass { // 成员内部类
-        private int i = 2;
+    public static int threeSumClosest(int[] nums, int target){
+        Arrays.sort(nums);
+        int abs = nums[0] + nums[1] + nums[2];
+        for (int i = 0; i < nums.length; i++) {
+            int p1 = i + 1;
+            int p2 = nums.length - 1;
+            while (p1 < p2) {
+                int sum = nums[i] + nums[p1] + nums[p2];
+                if (Math.abs(target - sum) < Math.abs(target - abs)) {
+                    abs = sum;
+                }
+                if (sum > target) {
+                    p2--;
+                } else if (sum < target) {
+                    p1++;
+                } else {
+                    return target;
+                }
+            }
 
-        public void innerMethod() {
-            int i = 3;
-            String str = "123";
-            System.out.println("i=" + i);
-            System.out.println("i=" + this.i);
-            System.out.println("i=" + test.this.i);
-            System.out.println("str=" + str);
         }
+        return abs;
     }
 
     public static void main(String[] args) {
-        //先创建外部类对象
-        test t = new test();
-        //创建内部类对象
-        test.InnerClass ti = t.new InnerClass();
-        //内部类对象调用自己的方法
-        ti.innerMethod();
+        System.out.println(ThreeSumClosest.threeSumClosest(new int[]{-1,2,1,-4},1));
     }
 }
