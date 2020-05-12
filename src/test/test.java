4d9d1893bebdfb76ec247ded2cbe1ebf72d7ea95
’rylynnczxshuaidai’ <477895675@qq.com>
2020-05-12 18:38:03 +0800

    5.12 快速排序

--- src/test/test.java
+++ src/test/test.java
@@ -2,35 +2,16 @@ package test;
 
 import Algorithms.ThreeSumClosest;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class test {
 
-    public static int threeSumClosest(int[] nums, int target){
-        Arrays.sort(nums);
-        int abs = nums[0] + nums[1] + nums[2];
-        for (int i = 0; i < nums.length; i++) {
-            int p1 = i + 1;
-            int p2 = nums.length - 1;
-            while (p1 < p2) {
-                int sum = nums[i] + nums[p1] + nums[p2];
-                if (Math.abs(target - sum) < Math.abs(target - abs)) {
-                    abs = sum;
-                }
-                if (sum > target) {
-                    p2--;
-                } else if (sum < target) {
-                    p1++;
-                } else {
-                    return target;
-                }
-            }
-
-        }
-        return abs;
-    }
-
     public static void main(String[] args) {
-        System.out.println(ThreeSumClosest.threeSumClosest(new int[]{-1,2,1,-4},1));
+        System.out.println(Integer.MAX_VALUE);
     }
 }
