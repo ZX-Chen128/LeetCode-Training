3af67a20e070275988a382d284452e01cd1465eb
’rylynnczxshuaidai’ <477895675@qq.com>
2020-09-01 20:45:37 +0800

    9.1 万得笔试题，尚未完成

---
+++ src/InterviewExperience/Wind/BiTree.java
@@ -0,0 +1,33 @@
+package InterviewExperience.Wind;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class BiTree {
+
+    public List<List<Integer>> count(TreeNode root, int K){
+        List<List<Integer>> answer = new ArrayList<>();
+        helper(answer,root,K);
+        System.out.println(answer);
+    }
+
+    public void helper(List<List<Integer>> answer,TreeNode root, int K){
+        if(root!=null){
+            ArrayList<Integer> addin = new ArrayList<>();
+            addin.add(root.val);
+        }
+    }
+
+}
+
+class TreeNode{
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x){
+        val=x;
+        left=null;
+        right=null;
+    }
+}
