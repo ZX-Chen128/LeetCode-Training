cd1e69630b3dc4a907451494bd95ac1e7369ccef
’rylynnczxshuaidai’ <477895675@qq.com>
2020-09-02 01:14:34 +0800

    9.1 LeetCode 138 一个深拷贝题，有些意思，对于深拷贝理解有帮助

---
+++ src/Algorithms/CopyListwithRandomPointer.java
@@ -0,0 +1,30 @@
+package Algorithms;
+
+import java.util.HashMap;
+
+public class CopyListwithRandomPointer {
+
+    HashMap<Node, Node> visitedHash = new HashMap<Node, Node>();
+
+    public Node copyRandomList(Node head) {
+
+        if (head == null) {
+            return null;
+        }
+
+        if (this.visitedHash.containsKey(head)) {
+            return this.visitedHash.get(head);
+        }
+
+        Node node = new Node(head.val, null, null);
+
+        this.visitedHash.put(head, node);
+
+        node.next = this.copyRandomList(head.next);
+        node.random = this.copyRandomList(head.random);
+
+        return node;
+    }
+
+}
+
--- src/Algorithms/Node.java
+++ src/Algorithms/Node.java
@@ -1,13 +1,16 @@
 package Algorithms;
 
 public class Node {
-    int val;
-    Node next;
-    Node random;
+    public int val;
+    public Node next;
+    public Node random;
 
-    public Node(int val) {
-        this.val = val;
-        this.next = null;
-        this.random = null;
+    public Node() {}
+
+    public Node(int _val,Node _next,Node _random) {
+        val = _val;
+        next = _next;
+        random = _random;
     }
+
 }
