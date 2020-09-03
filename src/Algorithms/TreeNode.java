1244012d0c738e4f3ce1f290ba695a7b666e87bd
陈子欣 <wb-wzl723959@@alibaba-inc.com>
2020-09-03 16:42:31 +0800

    9.3 引入一个TreeNode 和ListNode一样以后会经常被引用到

---
+++ src/Algorithms/TreeNode.java
@@ -0,0 +1,14 @@
+package Algorithms;
+
+/**
+ * @Author: Chen Zixin
+ * @Date: 2020/9/3 4:41 下午
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) {
+        val = x;
+    }
+}
