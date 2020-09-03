f975bef4e21530aa0904e091c693b5b47a242540
陈子欣 <wb-wzl723959@@alibaba-inc.com>
2020-09-03 16:38:02 +0800

    9.3 LeetCode 21 合并两个有序链表 第一个可以做出来的递归题 以栈的思想去思考还是很容易理解的

--- src/Algorithms/MergeTwoSortedLists.java
+++ src/Algorithms/MergeTwoSortedLists.java
@@ -6,28 +6,40 @@ package Algorithms;
  */
 
 public class MergeTwoSortedLists {
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode dummy = new ListNode(0);
-        ListNode head = dummy;
-        while(l1 != null && l2!= null){
-            if(l1.val <= l2.val){
-                head.next = l1;
-                l1 = l1.next;
-            } else {
-                head.next = l2;
-                l2 = l2.next;
-            }
-            head = head.next;
-        }
-        if(l1 != null){head.next = l1;}
-        if(l2 != null){head.next = l2;}
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
 
-        return dummy.next;
+        if(l1 == null){
+            return l2;
+        }
+
+        if(l2 == null){
+            return l1;
+        }
+
+        if(l1.val < l2.val){
+            l1.next = mergeTwoLists(l1.next,l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1,l2.next);
+            return l2;
+        }
     }
 
-    public class ListNode {
-        int val;
-        ListNode next;
-        ListNode(int x) { val = x; }
+    public static void main(String[] args) {
+        ListNode a1 = new ListNode(1);
+        ListNode a2 = new ListNode(2);
+        ListNode a3 = new ListNode(5);
+        a1.next = a2;
+        a2.next = a3;
+        ListNode b1 = new ListNode(0);
+        ListNode b2 = new ListNode(3);
+        ListNode b3 = new ListNode(4);
+        b1.next = b2;
+        b2.next = b3;
+        ListNode answer = MergeTwoSortedLists.mergeTwoLists(a1,b1);
+        while(answer!=null){
+            System.out.println(answer.val);
+            answer=answer.next;
+        }
     }
 }
