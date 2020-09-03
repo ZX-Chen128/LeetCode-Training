ca212b4d2c26e160376e0558bfac3e8ae44d5af0
陈子欣 <wb-wzl723959@@alibaba-inc.com>
2020-09-03 17:12:26 +0800

    9.3 LeetCode 101 对称二叉树 也是递归思想 每次往下的比较左右子树 逻辑并不负责

---
+++ src/Algorithms/SymmetricTree.java
@@ -0,0 +1,38 @@
+package Algorithms;
+
+/**
+ * @Author: Chen Zixin
+ * @Date: 2020/9/3 4:43 下午
+ */
+public class SymmetricTree {
+
+    public static boolean isSymmetric(TreeNode root) {
+        return isMirror(root, root);
+    }
+
+    public static boolean isMirror(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) return true;
+        if (t1 == null || t2 == null) return false;
+        return (t1.val == t2.val)
+                && isMirror(t1.right, t2.left)
+                && isMirror(t1.left, t2.right);
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        TreeNode t1 = new TreeNode(2);
+        TreeNode t2 = new TreeNode(2);
+        TreeNode t3 = new TreeNode(3);
+        TreeNode t4 = new TreeNode(4);
+        TreeNode t5 = new TreeNode(4);
+        TreeNode t6 = new TreeNode(3);
+        root.left = t1;
+        root.right = t2;
+        t1.left = t3;
+        t1.right = t4;
+        t2.left = t5;
+        t2.right = t6;
+        System.out.println(SymmetricTree.isSymmetric(root));
+    }
+
+}
