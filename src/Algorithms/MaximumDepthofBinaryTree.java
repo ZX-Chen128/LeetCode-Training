91178bf9907ddf4852e6926d79ee8dc3506f2188
陈子欣 <wb-wzl723959@@alibaba-inc.com>
2020-09-03 18:09:39 +0800

    9.3 LeetCode 104 二叉树最大深度 这个的递归思想比较容易想 二叉树最大深度就是左右二叉树的最大值再加一 一层一层往下找 直到NULL位置 深度为0 然后再一个一个往上递归返回 就可以加到这个二叉树的实际最大深度了

---
+++ src/Algorithms/MaximumDepthofBinaryTree.java
@@ -0,0 +1,53 @@
+package Algorithms;
+
+/**
+ * @Author: Chen Zixin
+ * @Date: 2020/9/3 5:15 下午
+ */
+
+/**
+ * 给定一个二叉树，找出其最大深度。
+ *
+ * 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+ *
+ * 说明: 叶子节点是指没有子节点的节点。
+ *
+ * 示例：
+ * 给定二叉树 [3,9,20,null,null,15,7]，
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * 返回它的最大深度 3 。
+ *
+ */
+
+public class MaximumDepthofBinaryTree {
+
+    public static int maxDepth(TreeNode root) {
+        if(root == null) {
+            return 0;
+        } else {
+            int left = maxDepth(root.left);
+            int right = maxDepth(root.right);
+            return Math.max(left, right) + 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        TreeNode t1 = new TreeNode(9);
+        TreeNode t2 = new TreeNode(20);
+        TreeNode t3 = new TreeNode(15);
+
+        root.left = t1;
+        root.right = t2;
+
+        t2.left = t3;
+
+        System.out.println(MaximumDepthofBinaryTree.maxDepth(root));
+    }
+
+}
