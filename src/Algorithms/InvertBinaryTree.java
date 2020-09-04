7cd1a8732984182b4a5b0c0b147f07a30f2a62c9
陈子欣 <wb-wzl723959@@alibaba-inc.com>
2020-09-04 10:32:46 +0800

    9.4 LeetCode 226 翻转二叉树 递归题越来越得心应手了 其实逻辑也很简单 我想我的树反转 就是左子树和右子树分别都翻转了 然后让我原来右结点指到左子树 左结点指到右子树 如果结点为null就返回null 10min轻松AC

---
+++ src/Algorithms/InvertBinaryTree.java
@@ -0,0 +1,47 @@
+package Algorithms;
+
+/**
+ * @Author: Chen Zixin
+ * @Date: 2020/9/4 10:21 上午
+ */
+
+/**
+ * 翻转一棵二叉树。
+ *
+ * 示例：
+ *
+ * 输入：
+ *
+ *      4
+ *    /   \
+ *   2     7
+ *  / \   / \
+ * 1   3 6   9
+ * 输出：
+ *
+ *      4
+ *    /   \
+ *   7     2
+ *  / \   / \
+ * 9   6 3   1
+ *
+ */
+
+public class InvertBinaryTree {
+
+    public TreeNode invertTree(TreeNode root) {
+
+        if(root == null){
+            return null;
+        }
+
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+
+        root.right = left;
+        root.left = right;
+
+        return root;
+    }
+
+}
