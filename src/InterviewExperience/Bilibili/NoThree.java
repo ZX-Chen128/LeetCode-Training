1eb21b39fb36120905df9dd14387f398224a09b4
’rylynnczxshuaidai’ <477895675@qq.com>
2020-09-04 23:35:43 +0800

    9.4 B站笔试 近期几个笔试题里最简单的一家了。。。

---
+++ src/InterviewExperience/Bilibili/NoThree.java
@@ -0,0 +1,50 @@
+package InterviewExperience.Bilibili;
+
+import java.util.Arrays;
+
+/**
+ * [[1, 2, 3, 4], [5, 6, 7, 8], [9,10,11,12] ]
+ *
+ * [1,2,3,4,8,12,11,10,9,5,6,7]
+ */
+
+public class NoThree {
+
+    public int[] SpiralMatrix(int[][] matrix) {
+
+        if (matrix == null || matrix.length == 0) {
+            return null;
+        }
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[] array = new int[m * n];
+        int i = 0;
+        int index = 0;
+
+        int count = (Math.min(m, n) + 1) / 2;
+
+        while (i < count) {
+            for (int j = i; j < n - i; j++) {
+                array[index++] = matrix[i][j];
+            }
+            for (int j = i + 1; j < m - i; j++) {
+                array[index++] = matrix[j][(n - 1) - i];
+            }
+            for (int j = (n - 1) - (i + 1); j >= i && (m - 1 - i != i); j--) {
+                array[index++] = matrix[(m - 1) - i][j];
+            }
+            for (int j = (m - 1) - (i + 1); j >= i + 1 && (n - 1 - i != i); j--) {
+                array[index++] = matrix[j][i];
+            }
+            i++;
+        }
+        return array;
+
+    }
+
+    public static void main(String[] args) {
+        int[] sum = new int[10];
+        System.out.println(Arrays.toString(sum));
+    }
+
+}
--- src/InterviewExperience/Bilibili/NoTwo.java
+++ src/InterviewExperience/Bilibili/NoTwo.java
@@ -1,4 +1,40 @@
 package InterviewExperience.Bilibili;
 
+/**
+ *  "aaabbaaac"
+ *
+ *   2
+ */
+
 public class NoTwo {
+    public static int GetFragment(String str) {
+        int index = 0;
+        int count = 0;
+        int[] nums = new int[str.length()];
+
+        while (index < str.length()) {
+            char standard = str.charAt(index);
+            int length = 0;
+            while (index != str.length() && str.charAt(index) == standard) {
+                length++;
+                index++;
+            }
+            count++;
+            nums[count - 1] = length;
+            if (length == 1) {
+                index++;
+            }
+        }
+        int sumUp = 0;
+
+        for (int i = 0; i < count; i++) {
+            sumUp += nums[i];
+        }
+
+        return sumUp / count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(GetFragment("aaabbaaac"));
+    }
 }
