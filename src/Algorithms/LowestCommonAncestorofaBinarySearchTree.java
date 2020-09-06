4f16015e50195be9fb8dd988ea5055e068a2859b
陈子欣 <wb-wzl723959@@alibaba-inc.com>
2020-09-06 14:14:31 +0800

    9.6 LeetCode 235 二叉搜索树的最近公共祖先 这题是在T236之后做的，但是其实更简单得多，逻辑很简单。因为二叉搜索树有左子树小右子树大的特征，而最近公共祖先的特征是如果我左子树有一个，右子树有一个，我一定是最大公共祖先。因此，得出这样的逻辑，如果两个target都比我小，那我就在左子树（小的）那里找，如果都比我大，就在右子树（大的）那里找。其他情况直接返回，我即祖先。

---
+++ src/Algorithms/LowestCommonAncestorofaBinarySearchTree.java
@@ -0,0 +1,34 @@
+package Algorithms;
+
+/**
+ * @Author: Chen Zixin
+ * @Date: 2020/9/6 1:39 下午
+ */
+
+/**
+ * 示例 1:
+ * <p>
+ * 输入: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+ * 输出: 6
+ * 解释: 节点 2 和节点 8 的最近公共祖先是 6。
+ * 示例 2:
+ * <p>
+ * 输入: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+ * 输出: 2
+ * 解释: 节点 2 和节点 4 的最近公共祖先是 2, 因为根据定义最近公共祖先节点可以为节点本身。
+ */
+
+public class LowestCommonAncestorofaBinarySearchTree {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        if (root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        } else if (root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+
+        return root;
+    }
+
+}
