2451f2239fc89c416e0717aa870021951b00969d
陈子欣 <wb-wzl723959@@alibaba-inc.com>
2020-09-06 13:23:06 +0800

    9.6 LeetCode 236 二叉树的最近公共祖先 逻辑依据在于 如果我在左右子树各能找到两个target 那我必然是其最近公共祖先 若有一边为null 那最近公共祖先一定在不为null那侧 依次递归寻找即可 此题与T235相关 马上可以做一下

---
+++ src/Algorithms/LowestCommonAncestorofaBinaryTree.java
@@ -0,0 +1,29 @@
+package Algorithms;
+
+/**
+ * @Author: Chen Zixin
+ * @Date: 2020/9/5 8:22 下午
+ */
+public class LowestCommonAncestorofaBinaryTree {
+
+    public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        if (root == null) return null;
+        if (root == p || root == q) {
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left != null && right != null) {
+            return root;
+        } else if (left == null) {
+            return right;
+        } else {
+            return left;
+        }
+
+    }
+
+}
