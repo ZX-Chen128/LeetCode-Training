95d64373e4d589671b9ec33d3f4a880abe3a13fc
陈子欣 <wb-wzl723959@@alibaba-inc.com>
2020-09-09 10:41:58 +0800

    9.9 LeetCode 35 第二次重刷算法时候写到的 这次自己用了二分法实现了 很清晰 如果有直接返回mid下标 如果没有那么一直到搜索结束的start正是要插入的index

--- src/Algorithms/SearchInsertPosition.java
+++ src/Algorithms/SearchInsertPosition.java
@@ -1,24 +1,49 @@
 package Algorithms;
 
+/**
+ * 示例 1:
+ * <p>
+ * 输入: [1,3,5,6], 5
+ * 输出: 2
+ * 示例 2:
+ * <p>
+ * 输入: [1,3,5,6], 2
+ * 输出: 1
+ * 示例 3:
+ * <p>
+ * 输入: [1,3,5,6], 7
+ * 输出: 4
+ * 示例 4:
+ * <p>
+ * 输入: [1,3,5,6], 0
+ * 输出: 0
+ */
+
 public class SearchInsertPosition {
-    public int searchInsert(int[] nums, int target) {
+    public static int searchInsert(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
 
-        int index = -1;
+        int mid;
 
-        if (!(target > nums[0])){
-            return 0;
-        }
-        if(target > nums[nums.length-1]){
-            return nums.length;
-        }
+        while (start <= end) {
+            mid = (start + end) / 2;
 
-        for (int i = 0 ; i < nums.length ; i++){
-            if(target > nums[i] && !(target > nums[i+1])){
-                index = i + 1;
+            if(nums[mid] == target){
+                return mid;
+            } else if(nums[mid] > target){
+                end = mid - 1;
+            } else {
+                start = mid + 1;
             }
         }
+        return start;
+    }
 
-        return index;
-
+    public static void main(String[] args) {
+        System.out.println(searchInsert(new int[]{1,3,5,6},5));
+        System.out.println(searchInsert(new int[]{1,3,5,6},2));
+        System.out.println(searchInsert(new int[]{1,3,5,6},7));
+        System.out.println(searchInsert(new int[]{1,3,5,6},0));
     }
 }
