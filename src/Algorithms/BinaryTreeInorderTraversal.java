07116ab871c268e5a3e928f60a6665b7dabf3afc
陈子欣 <wb-wzl723959@@alibaba-inc.com>
2020-09-15 15:24:16 +0800

    9.15 LeetCode 94 写了两条回溯再来写这条 其实和递归的道理一致 其实递归也是一种带剪枝的遍历 二叉树里遇到null便剪枝

--- src/Algorithms/BinaryTreeInorderTraversal.java
+++ src/Algorithms/BinaryTreeInorderTraversal.java
@@ -9,33 +9,36 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * 给定一个二叉树，返回它的中序 遍历。
+ *
  * 示例:
- * <p>
+ *
  * 输入: [1,null,2,3]
- * 1
- * \
- * 2
- * /
- * 3
- * <p>
+ *    1
+ *     \
+ *      2
+ *     /
+ *    3
+ *
  * 输出: [1,3,2]
+ *
  */
 
 public class BinaryTreeInorderTraversal {
 
     public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> list = new ArrayList<>();
-        helper(root, list);
-        return list;
+        List<Integer> lists = new ArrayList<>();
+        dfs(root, lists);
+        return lists;
     }
 
-    public void helper(TreeNode root, List<Integer> list) {
+    public void dfs(TreeNode root, List<Integer> lists) {
         if (root == null) {
             return;
         }
-        helper(root.left, list);
-        list.add(root.val);
-        helper(root.right, list);
+        dfs(root.left, lists);
+        lists.add(root.val);
+        dfs(root.right, lists);
     }
 
 }
