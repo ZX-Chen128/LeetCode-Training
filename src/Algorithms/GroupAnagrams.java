e15335ff4a99aee1704b3c5c0b466df9aab95258
’rylynnczxshuaidai’ <477895675@qq.com>
2021-01-02 16:31:01 +0800

    1.2 LeetCode 49 很简单的思想，通过map来分类，相同字母的String在toCharArray()之后再排序是同一个，也应该是同一类，把这个设为key，value存答案的List，遍历String[]后即为所求

---
+++ src/Algorithms/GroupAnagrams.java
@@ -0,0 +1,30 @@
+package Algorithms;
+
+import java.util.*;
+
+/**
+ * 示例:
+ * <p>
+ * 输入: ["eat", "tea", "tan", "ate", "nat", "bat"]
+ * 输出:
+ * [
+ * ["ate","eat","tea"],
+ * ["nat","tan"],
+ * ["bat"]
+ * ]
+ */
+
+public class GroupAnagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            List<String> list = map.getOrDefault(key, new ArrayList<>());
+            list.add(str);
+            map.put(key, list);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
