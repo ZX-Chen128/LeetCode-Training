345ff70bcf921925bd175eb66a81ec6886e2ec75
’rylynnczxshuaidai’ <477895675@qq.com>
2021-01-14 16:47:57 +0800

    1.14 LeetCode 75 看了题解知道这个叫做partition循环不变量。其实和快排很类似，找到一个基准点（这题就是1），然后根据基准点可以排序好两边（0和2），一次遍历O(n)就可以将所有数找到它们应在的位置

---
+++ src/Algorithms/SortColors.java
@@ -0,0 +1,51 @@
+package Algorithms;
+
+/**
+ * 示例 1：
+ * <p>
+ * 输入：nums = [2,0,2,1,1,0]
+ * 输出：[0,0,1,1,2,2]
+ * 示例 2：
+ * <p>
+ * 输入：nums = [2,0,1]
+ * 输出：[0,1,2]
+ * 示例 3：
+ * <p>
+ * 输入：nums = [0]
+ * 输出：[0]
+ * 示例 4：
+ * <p>
+ * 输入：nums = [1]
+ * 输出：[1]
+ */
+
+public class SortColors {
+    public void sortColors(int[] nums) {
+        int len = nums.length;
+
+        int zero = 0;
+        int two = len - 1;
+
+        int i = 0;
+
+        while (i <= two) {
+            if (nums[i] == 0) {
+                swap(nums, i, zero);
+                i++;
+                zero++;
+            } else if (nums[i] == 1) {
+                i++;
+            } else {
+                swap(nums, i, two);
+                two--;
+            }
+        }
+    }
+
+    private void swap(int[] nums, int index1, int index2) {
+        int temp = nums[index1];
+        nums[index1] = nums[index2];
+        nums[index2] = temp;
+    }
+
+}
