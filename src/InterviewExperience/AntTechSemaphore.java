d14a58edb5bf2fbc2996ce461607438a47ab0598
’rylynnczxshuaidai’ <477895675@qq.com>
2021-04-13 22:35:53 +0800

    4.13 蚂蚁金服面试题 多线程与Semaphore的问题 自解答案

--- src/InterviewExperience/AntTechSemaphore.java
+++ src/InterviewExperience/AntTechSemaphore.java
@@ -5,44 +5,43 @@ import java.util.concurrent.Semaphore;
 //题目2:
 //有三个线程ID分别是A、B、C,请用多线编程实现，在屏幕上循环打印10次ABC
 public class AntTechSemaphore {
+    static Semaphore s1 = new Semaphore(1);
+    static Semaphore s2 = new Semaphore(0);
+    static Semaphore s3 = new Semaphore(0);
+
     public static void main(String[] args) {
-        PrintABCUsingSemaphore printABC = new PrintABCUsingSemaphore();
-        new Thread(() -> printABC.printA()).start();
-        new Thread(() -> printABC.printB()).start();
-        new Thread(() -> printABC.printC()).start();
-    }
-}
-
-class PrintABCUsingSemaphore {
-    private Semaphore semaphoreA = new Semaphore(1);
-    private Semaphore semaphoreB = new Semaphore(0);
-    private Semaphore semaphoreC = new Semaphore(0);
-    //private int attempts = 0;
-
-
-    public void printA() {
-        print("A", semaphoreA, semaphoreB);
+        printABC();
     }
 
-    public void printB() {
-        print("B", semaphoreB, semaphoreC);
-    }
-
-    public void printC() {
-        print("C", semaphoreC, semaphoreA);
-    }
-
-    private void print(String name, Semaphore currentSemaphore, Semaphore nextSemaphore) {
-        for (int i = 0; i < 10; ) {
+    public static void printABC() {
+        new Thread(() -> {
             try {
-                currentSemaphore.acquire();
-                //System.out.println(Thread.currentThread().getName()+" try to print "+name+", attempts : "+(++attempts));
-                System.out.println(Thread.currentThread().getName() +" print "+ name);
-                i++;
-                nextSemaphore.release();
+                print("A", s1, s2);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+        }).start();
+        new Thread(() -> {
+            try {
+                print("B", s2, s3);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+        new Thread(() -> {
+            try {
+                print("C", s3, s1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
+    public static void print(String name, Semaphore nowSemaphore, Semaphore nextSemaphore) throws InterruptedException {
+        for (int i = 1; i <= 10; i++) {
+            nowSemaphore.acquire();
+            System.out.println(Thread.currentThread().getName() + "  " +name + "  第" + i + "次");
+            nextSemaphore.release();
         }
     }
 }
