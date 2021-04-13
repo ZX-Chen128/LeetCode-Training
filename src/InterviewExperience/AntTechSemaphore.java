117acf73d8098d463f78071edfcf8f7f7055f3dd
zixinchen <zixinchen@trip.com>
2021-04-13 18:38:08 +0800

    Semaphore print ABC

--- src/InterviewExperience/AntTechSemaphore.java
+++ src/InterviewExperience/AntTechSemaphore.java
@@ -1,59 +1,48 @@
 package InterviewExperience;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
 
 //题目2:
 //有三个线程ID分别是A、B、C,请用多线编程实现，在屏幕上循环打印10次ABC
 public class AntTechSemaphore {
+    public static void main(String[] args) {
+        PrintABCUsingSemaphore printABC = new PrintABCUsingSemaphore();
+        new Thread(() -> printABC.printA()).start();
+        new Thread(() -> printABC.printB()).start();
+        new Thread(() -> printABC.printC()).start();
+    }
+}
 
-    public static void multiThread() {
-        Thread A = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("A");
+class PrintABCUsingSemaphore {
+    private Semaphore semaphoreA = new Semaphore(1);
+    private Semaphore semaphoreB = new Semaphore(0);
+    private Semaphore semaphoreC = new Semaphore(0);
+    //private int attempts = 0;
+
+
+    public void printA() {
+        print("A", semaphoreA, semaphoreB);
+    }
+
+    public void printB() {
+        print("B", semaphoreB, semaphoreC);
+    }
+
+    public void printC() {
+        print("C", semaphoreC, semaphoreA);
+    }
+
+    private void print(String name, Semaphore currentSemaphore, Semaphore nextSemaphore) {
+        for (int i = 0; i < 10; ) {
+            try {
+                currentSemaphore.acquire();
+                //System.out.println(Thread.currentThread().getName()+" try to print "+name+", attempts : "+(++attempts));
+                System.out.println(Thread.currentThread().getName() +" print "+ name);
+                i++;
+                nextSemaphore.release();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-        });
-
-        Thread B = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    A.join();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                System.out.println("B");
-            }
-        });
-
-        Thread C = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    B.join();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                System.out.println("C");
-            }
-        });
-
-        ExecutorService poolExecutor = Executors.newSingleThreadExecutor();
-
-        poolExecutor.submit(A);
-        poolExecutor.submit(B);
-        poolExecutor.submit(C);
-
-        for (int i = 0; i < 10; i++) {
-            A.start();
-            B.start();
-            C.start();
         }
     }
-
-    public static void main(String[] args) {
-        multiThread();
-    }
-
 }
